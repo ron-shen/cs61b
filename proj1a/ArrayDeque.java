4bf3e7a65a84186012ad3ea2cb0ba30afd45b5a1
ron shen <ronshen0404@gmail.com>
2023-09-23 21:31:08 -0400

    first commit

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,79 @@
+package project1.deque;
+
+
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+    private int head;
+    private int tail;
+    ArrayDeque(){
+        items = (T[]) new Object[8];
+        size = 0;
+        head = 0;
+        tail = 0;
+    }
+    private void resize(int capacity){
+        int idx = 0;
+        T[] newItems = (T[]) new Object[capacity];
+        for(int i = 0; i < size; i++){
+            idx = (head + i) % items.length;
+            newItems[i] = items[idx];
+        }
+        items = newItems;
+        head = 0;
+        tail = size;
+    }
+    public void addFirst(T item){
+        if(size == items.length){
+            resize(size * 2);
+        }
+        head = head - 1 < 0 ? items.length - 1 : head - 1;
+        items[head] = item;
+        size += 1;
+    }
+    public void addLast(T item){
+        if(size == items.length){
+            resize(size * 2);
+        }
+        items[tail] = item;
+        tail = (tail + 1) % items.length;
+        size += 1;
+    }
+    public boolean isEmpty(){
+        return size != 0 ? true : false;
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+        int idx = 0;
+        for(int i = 0; i < size; i++){
+            idx = (head + i) % items.length;
+            System.out.print(items[idx]);
+            System.out.print(' ');
+        }
+    }
+    public T removeFirst(){
+        T item = items[head];
+        items[head] = null;
+        head = head + 1 >= items.length ? 0 : head + 1;
+        size -= 1;
+        if(items.length >= 16 && (double) size / items.length < 0.25){
+            resize(items.length / 2);
+        }
+        return item;
+    }
+    public T removeLast(){
+        tail = tail - 1 < 0 ? items.length - 1 : tail - 1;
+        T item = items[tail];
+        items[tail] = null;
+        size -= 1;
+        if(items.length >= 16 && (double) size / items.length < 0.25){
+            resize(items.length / 2);
+        }
+        return item;
+    }
+    public T get(int index){
+        return items[(head + index) % items.length];
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,184 @@
+package project1.deque;
+
+
+class Node<T>{
+    public T val;
+    public Node<T> next;
+    public Node<T> previous;
+    public Node(T val, Node<T> next, Node<T> previous) {
+        this.val = val;
+        this.next = next;
+        this.previous = previous;
+    }
+}
+
+//implement doubly linked list using first and last pointer only.
+//no Sentinel node
+class LinkedList<T>{
+    private Node first;
+    private Node last;
+    public LinkedList(){
+        this.first = null;
+        this.last = null;
+    }
+
+    public void addFirst(T val){
+        Node newNode = new Node(val, null, null);
+        //it needs to create an edge case when Linked List is empty
+        if(first == null && last == null){
+            first = newNode;
+            last = newNode;
+            return;
+        }
+        newNode.next = first;
+        first.previous = newNode;
+        first = newNode;
+    }
+
+    public void addLast(T val){
+        Node newNode = new Node(val, null, null);
+        //it needs to create an edge case when Linked List is empty
+        if(first == null && last == null){
+            first = newNode;
+            last = newNode;
+            return;
+        }
+        newNode.previous = last;
+        last.next = newNode;
+        last = newNode;
+    }
+
+    public void removeFirst(){
+        //many edge cases
+        if(first == null && last == null){
+            return;
+        }
+        //one node only
+        if(first == last){
+            first = null;
+            last = null;
+        }
+        //more than one node
+        else{
+            first = first.next;
+            first.previous = null;
+        }
+    }
+
+    public void removeLast(){
+        //many edge cases
+        if(first == null && last == null){
+            return;
+        }
+        //one node only
+        if(first == last){
+            first = null;
+            last = null;
+        }
+        else{
+            last = last.previous;
+            last.next = null;
+        }
+
+    }
+
+    public Node getFirst(){
+        return first;
+    }
+
+    public Node getLast(){
+        return last;
+    }
+}
+
+public class LinkedListDeque<T> {
+    private Node<T> header;
+    private Node<T> trailer;
+    private int size;
+    public LinkedListDeque(){
+        this.header = new Node<T>(null, null, null);
+        this.trailer = new Node<T>(null, null, header);
+        header.next = this.trailer;
+        this.size = 0;
+    }
+    public void addFirst(T item){
+        Node newNode = new Node<T>(item, null, null);
+        newNode.previous = header;
+        newNode.next = header.next;
+        header.next.previous = newNode;
+        header.next = newNode;
+        size += 1;
+    }
+    public void addLast(T item){
+        Node<T> newNode = new Node<T>(item, null, null);
+        newNode.next = trailer;
+        newNode.previous = trailer.previous;
+        trailer.previous.next = newNode;
+        trailer.previous = newNode;
+        size += 1;
+    }
+    public boolean isEmpty(){
+        return size == 0 ? true : false;
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+        Node<T> temp = header.next;
+        while(temp != trailer){
+            System.out.print(temp.val);
+            System.out.print(' ');
+            temp = temp.next;
+        }
+    }
+    public T removeFirst(){
+        if(size == 0){
+            return null;
+        }
+        Node<T> temp = header.next;
+        header.next.next.previous = header;
+        header.next = header.next.next;
+        temp.previous = null;
+        temp.next = null;
+        size -= 1;
+        return temp.val;
+    }
+    public T removeLast(){
+        if(size == 0){
+            return null;
+        }
+        Node<T> temp = trailer.previous;
+        trailer.previous.previous.next = trailer;
+        trailer.previous = trailer.previous.previous;
+        temp.next = null;
+        temp.previous = null;
+        size -= 1;
+        return temp.val;
+    }
+    public T get(int index){
+        if(size == 0 || index >= size || index < 0){
+            return null;
+        }
+        int count = 0;
+        Node<T> temp = header.next;
+        while(count != index){
+            count++;
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+
+    private T getRecursiveHelper(Node<T> temp, int index){
+        if(index == 0){
+            return temp.val;
+        }
+        return getRecursiveHelper(temp.next, index - 1);
+    }
+    public T getRecursive(int index){
+        if(size == 0 || index >= size || index < 0){
+            return null;
+        }
+        return getRecursiveHelper(header.next, index);
+    }
+}
+
