bdc1d69d387edd4784d3e99c1ef2210ad3357127
ron shen <ronshen0404@gmail.com>
2023-10-16 17:08:44 -0400

    hw3

--- hw3/hash/OomageTestUtility.java
+++ hw3/hash/OomageTestUtility.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
+
         /* TODO:
          * Write a utility function that returns true if the given oomages
          * have hashCodes that would distribute them fairly evenly across
@@ -12,6 +13,18 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int[] buckets = new int[M];
+        int totalOomages = oomages.size();
+        for(int i = 0; i < totalOomages; i++){
+            Oomage o = oomages.get(i);
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            buckets[bucketNum] += 1;
+        }
+        for(int i = 0; i < buckets.length; i++){
+            if((buckets[i] < totalOomages / 50) || (buckets[i] > totalOomages / 2.5)){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- hw3/hash/SimpleOomage.java
+++ hw3/hash/SimpleOomage.java
@@ -3,6 +3,8 @@ import java.awt.Color;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdDraw;
 
+import static java.lang.Math.pow;
+
 
 public class SimpleOomage implements Oomage {
     protected int red;
@@ -10,26 +12,27 @@ public class SimpleOomage implements Oomage {
     protected int blue;
 
     private static final double WIDTH = 0.01;
-    private static final boolean USE_PERFECT_HASH = false;
+    private static final boolean USE_PERFECT_HASH = true;
 
     @Override
     public boolean equals(Object o) {
-        // TODO: Write this method.
-        return false;
+        if (o.getClass() != this.getClass()) return false;
+        SimpleOomage obj = (SimpleOomage) o;
+        return this.red == obj.red &&
+                this.green == obj.green &&
+                this.blue == obj.blue;
     }
 
-    /* Uncomment this method after you've written
-       equals and failed the testHashCodeAndEqualsConsistency
-       test.
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
             // TODO: Write a perfect hash function for Simple Oomages.
-            return 0;
+            int val = (int) ((red / 5) * pow(31, 2) + (green / 5) * pow(31, 1) + (blue / 5) * pow(31, 0));
+            return val;
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
